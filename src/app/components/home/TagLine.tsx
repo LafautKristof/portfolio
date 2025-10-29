e4ee4815125ee4b53728e78453c8d521f660bec6
Sjabloons <kristoflafaut@gmail.com>
2025-10-29 22:35:19 +0100

    save

--- src/app/components/home/TagLine.tsx
+++ src/app/components/home/TagLine.tsx
@@ -1,31 +1,24 @@
 "use client";
 import { motion } from "framer-motion";
 import { title, text } from "@/app/helpers/homeVars";
+import { fadeIn } from "@/app/helpers/motionVariants";
 const TagLine = () => {
     return (
         <>
             {" "}
             <div>
                 <motion.div
-                    initial={{ opacity: 0, scale: 0.8 }}
-                    whileInView={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.6, ease: "easeOut" }}
-                    viewport={{ once: true, amount: 0.5 }}
                     className="text-center flex flex-col gap-5 max-w-4xl mx-auto text-white"
                     style={{ fontFamily: "var(--font-quantico)" }}
+                    {...fadeIn}
                 >
                     <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold leading-tight">
                         {title}
                     </h1>
                 </motion.div>
                 <motion.div
-                    initial={{ opacity: 0, scale: 0.8 }}
-                    whileInView={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.6, ease: "easeOut" }}
-                    viewport={{ once: true, amount: 0.5 }}
-                    // once:false = opnieuw afspelen bij scroll
-                    // amount:0.5 = animatie start als 50% zichtbaar is
                     className="flex flex-col justify-center"
+                    {...fadeIn}
                 >
                     <p className="max-w-2xl mx-auto text-center text-lg sm:text-xl text-gray-300">
                         {text}
